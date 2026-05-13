ea3543f4dc21b6c6cca8f554d0ac9f6d20d01dc2
Yaroslav Goncharov <yarmaks07@gmail.com>
2026-05-13 16:48:56 +0300

    Исправленая game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -4,12 +4,11 @@
 #include "raylib.h"
 
 constexpr int CELL=32;
-
 constexpr int EX = 690;
 constexpr int EY = 100;
-
-static float enemyTimer = 0.0f;
 constexpr float ENEMY_DELAY = 0.5f;
+
+static Bot bot;
 
 void ResetGame(GameState& g, std::mt19937& r){
     g.playerBoard=CreateBoard();
@@ -19,6 +18,8 @@ void ResetGame(GameState& g, std::mt19937& r){
     g.turn=Turn::Player;
     g.gameOver=false;
     g.playerWon=false;
+    g.enemyTimer=0.0f;
+    bot=Bot();
 }
 
 bool MouseToCell(int mx,int my,int bx,int by,Point& out){
@@ -26,38 +27,47 @@ bool MouseToCell(int mx,int my,int bx,int by,Point& out){
     int x=(mx-bx)/CELL;
     int y=(my-by)/CELL;
     if(!InBounds(x,y)) return false;
-    out={x,y}; return true;
+    out={x,y}; 
+    return true;
 }
 
 void HandlePlayerTurn(GameState& g){
     if(!IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) return;
     Point p;
     if(!MouseToCell(GetMouseX(),GetMouseY(),EX,EY,p)) return;
+
     Result result = Shoot(g.enemyBoard,p);
     if(result == Result::Invalid) return;
+
     if(CountAliveDecks(g.enemyBoard)==0){
-        g.gameOver=true; g.playerWon=true; return;
+        g.gameOver=true; 
+        g.playerWon=true; 
+        return;
     }
+
     if (result == Result::Miss)
         g.turn=Turn::Enemy;
 }
 
 void HandleEnemyTurn(GameState& g,std::mt19937& r){
-    enemyTimer += GetFrameTime();
+    g.enemyTimer+=GetFrameTime();
+    if (g.enemyTimer < ENEMY_DELAY) return;
+    g.enemyTimer = 0.0f;
 
-    if (enemyTimer < ENEMY_DELAY)
-        return;
-
-    enemyTimer = 0.0f;
     Result result = Result::Invalid;
+    Point shot;
+
+
     do{
-        Point p = GetBotShot();
-        result = Shoot(g.playerBoard,p);
-        SetBotResult(result);
+        shot  = GetBotShot(bot, g.playerBoard);
+        result = Shoot(g.playerBoard, shot);
+        SetBotResult(bot, result, shot);
     }while (result == Result::Invalid);
     
     if(CountAliveDecks(g.playerBoard)==0){
-        g.gameOver=true; g.playerWon=false; return;
+        g.gameOver=true; 
+        g.playerWon=false; 
+        return;
     }
 
     if (result == Result::Miss)
